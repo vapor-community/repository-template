6bce6b706bbcac0b0d6cdd65b12b8c425b444450
Gustavo Perdomo <gperdomor@gmail.com>
2018-08-02 16:39:10 -0400

    Initial POC

---
+++ Sources/App/Controllers/API/TodoController.swift
@@ -0,0 +1,25 @@
+import Vapor
+
+/// Controls basic CRUD operations on `Todo`s.
+final class TodoController {
+    /// Returns a list of all `Todo`s.
+    func index(_ req: Request) throws -> Future<[Todo]> {
+        let repository = try req.make(TodoRepository.self)
+        return repository.findAll(on: req)
+    }
+
+    /// Saves a decoded `Todo` to the database.
+    func create(_ req: Request) throws -> Future<Todo> {
+        return try req.content.decode(Todo.self).flatMap { todo in
+            let repository = try req.make(TodoRepository.self)
+            return repository.save(model: todo, on: req)
+        }
+    }
+
+    /// Deletes a parameterized `Todo`.
+    func delete(_ req: Request) throws -> Future<HTTPStatus> {
+        return try req.parameters.next(Todo.self).flatMap { todo in
+            return todo.delete(on: req)
+        }.transform(to: .ok)
+    }
+}
